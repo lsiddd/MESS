0a886f6b3a1b01feb64029a82939cbaff00ba10b
lsiddd <lucassidpacheco@gmail.com>
2019-09-04 16:07:41 -0300

    coordinates calculation ypdate

--- make_trace.py
+++ make_trace.py
@@ -6,15 +6,15 @@ from math import sin, cos
 def main():
     R = 6371
 
-    number_of_users = 100
+    number_of_users = 200
     number_of_samples = 1000
 
     filenames = glob("new*.txt")
     user = 0
     outfile = open("SanFrancisco.tcl", "w")
 
-    # x_plot = []
-    # y_plot = []
+    x_plot = []
+    y_plot = []
 
     for filename in filenames[:number_of_users]:
         print(f"opening file: {filename}")
@@ -22,20 +22,22 @@ def main():
         with open(filename) as f:
             lines = [i.split() for i in f.readlines()[:number_of_samples] ]
 
-            # x_plot.append( R * cos(float(lines[0][0])) * cos(float(lines[0][1])))
-            # y_plot.append( R * cos(float(lines[0][0])) * sin(float(lines[0][1])))
+            x_pos = R * cos(float(lines[0][0])) * cos(float(lines[0][1])) + 6300
+            y_pos = R * cos(float(lines[0][0])) * sin(float(lines[0][1])) + 800
 
             second = 0
 
-            outfile.write("$node_({}) set X_ {}\n".format(user, lines[0][0]))
-            outfile.write("$node_({}) set Y_ {}\n".format(user, lines[0][1]))
+            outfile.write("$node_({}) set X_ {}\n".format(user, x_pos))
+            outfile.write("$node_({}) set Y_ {}\n".format(user, y_pos))
             outfile.write("$node_({}) set Z_ 1\n".format(user))
 
             for line in lines:
-                # x_plot.append( R * cos(float(line[0])) * cos(float(line[1])))
-                # y_plot.append( R * cos(float(line[0])) * sin(float(line[1])))
+                x_pos = R * cos(float(line[0])) * cos(float(line[1])) + 6300
+                y_pos = R * cos(float(line[0])) * sin(float(line[1])) + 800
+                x_plot.append(x_pos)
+                y_plot.append(y_pos)
 
-                outfile.write(f'$ns_ at {float(second)} "$node_({user}) setdest {R * cos(float(line[0])) * cos(float(line[1]))} {R * cos(float(line[0])) * sin(float(line[1]))} 1"\n')
+                outfile.write(f'$ns_ at {float(second)} "$node_({user}) setdest {x_pos} {y_pos} 1"\n')
 
                 second += 1
 
@@ -43,9 +45,9 @@ def main():
 
     outfile.close()
 
-    # plt.figure()
-    # plt.scatter(x_plot, y_plot)
-    # plt.show()
+    plt.figure()
+    plt.scatter(x_plot, y_plot)
+    plt.show()
 
 if __name__ == "__main__":
     main()
