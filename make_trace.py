0418e3d509de6c4d5e5ec5dc4fd176716dae0857
lsiddd <lucassidpacheco@gmail.com>
2019-09-10 15:33:10 -0300

    mobility coordinates changes

--- make_trace.py
+++ make_trace.py
@@ -7,9 +7,9 @@ import random
 def main():
     R = 6371
 
-    number_of_users = 200
-    number_of_samples = 1000
-    number_of_cells = 200
+    number_of_users = 50
+    number_of_samples = 100
+    number_of_cells = 100
 
     filenames = glob("new*.txt")
     user = 0
@@ -24,8 +24,8 @@ def main():
         with open(filename) as f:
             lines = [i.split() for i in f.readlines()[:number_of_samples] ]
 
-            x_pos = R * cos(float(lines[0][0])) * cos(float(lines[0][1])) + 6300
-            y_pos = R * cos(float(lines[0][0])) * sin(float(lines[0][1])) + 800
+            x_pos = R * cos(float(lines[0][0])) * cos(float(lines[0][1]))
+            y_pos = R * cos(float(lines[0][0])) * sin(float(lines[0][1])) * 10
 
             # x_plot.append(x_pos)
             # y_plot.append(y_pos * 10)
@@ -33,14 +33,20 @@ def main():
             second = 0
 
             outfile.write("$node_({}) set X_ {}\n".format(user, x_pos))
-            outfile.write("$node_({}) set Y_ {}\n".format(user, y_pos * 10))
+            outfile.write("$node_({}) set Y_ {}\n".format(user, y_pos))
             outfile.write("$node_({}) set Z_ 1\n".format(user))
 
             for line in lines:
-                x_pos = R * cos(float(line[0])) * cos(float(line[1])) + 6300
-                y_pos = R * cos(float(line[0])) * sin(float(line[1])) + 800
+                x_pos = R * cos(float(line[0])) * cos(float(line[1]))
+                y_pos = R * cos(float(line[0])) * sin(float(line[1])) * 10
+
+                if (x_pos < -6380 or x_pos > 6280):
+                    continue
+                if (y_pos > -1000 or y_pos < -9000 ):
+                    continue
+
                 x_plot.append(x_pos)
-                y_plot.append(y_pos * 10)
+                y_plot.append(y_pos)
 
                 outfile.write(f'$ns_ at {float(second)} "$node_({user}) setdest {x_pos} {y_pos} 1"\n')
 
@@ -52,13 +58,20 @@ def main():
 
     cellsFile = open("cellsDataset", "w")
     id = 1
+    x = []
+    y = []
     for i in random.sample(list(zip(x_plot, y_plot)), number_of_cells):
-        cellsFile.write(f"{id} {i[0] + random.uniform(1,50)} {i[1] + random.uniform(1,50)}\n")
+        x.append( i[0] + random.uniform(-50,50) )
+        y.append( i[1] + random.uniform(-400,300) )
+        cellsFile.write(f"{id} {x[-1]} {y[-1]}\n")
         id += 1
     cellsFile.close()
 
+    print (np.mean(x_plot))
+    print (np.mean(y_plot))
     plt.figure()
     plt.scatter(x_plot, y_plot)
+    plt.scatter(x, y, color="green")
     plt.show()
 
 if __name__ == "__main__":
